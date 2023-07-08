5cfb0cc03dc7fd3e0c8d1c663a001f2192b846ed
Ross Hammond <62562952+rossdhammond@users.noreply.github.com>
2023-07-08 14:57:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   // Get the bottom window element
   const bottomWindow = document.getElementById('bottom-window');
 
-  // Get all the buttons
-  const buttons = document.querySelectorAll('.interactive-window button');
+  // Get the audio controls
+  const playButton = document.getElementById('play-button');
+  const pauseButton = document.getElementById('pause-button');
+  const genreDropdown = document.getElementById('genre-dropdown');
+  const audio = new Audio();
 
   // Function to change the bottom window image
   function changeImage(imagePath) {
@@ -11,35 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
     imageElement.src = imagePath;
   }
 
+  // Function to play audio based on genre selection
+  function playAudio(genre) {
+    let audioSrc = '';
+    switch (genre) {
+      case 'pop':
+        audioSrc = 'pop-music.mp3';
+        break;
+      case 'musical':
+        audioSrc = 'musical-music.mp3';
+        break;
+      case 'jazz':
+        audioSrc = 'jazz-music.mp3';
+        break;
+      default:
+        return;
+    }
+    audio.src = audioSrc;
+    audio.play();
+  }
+
   // Add click event listener to each button
-  buttons.forEach(button => {
-    button.addEventListener('click', function() {
-      // Determine which button was clicked
-      const buttonId = this.id;
-      
-      // Change the image based on the button clicked
-      switch (buttonId) {
-        case 'button1':
-          changeImage('image2.jpg');
-          break;
-        case 'button2':
-          changeImage('image3.jpg');
-          break;
-        case 'button3':
-          changeImage('image4.jpg');
-          break;
-        case 'button4':
-          changeImage('image5.jpg');
-          break;
-        case 'button5':
-          changeImage('image6.jpg');
-          break;
-        case 'button6':
-          changeImage('image7.jpg');
-          break;
-        default:
-          changeImage('default-image.jpg');
-      }
-    });
+  playButton.addEventListener('click', function() {
+    audio.play();
+  });
+
+  pauseButton.addEventListener('click', function() {
+    audio.pause();
+  });
+
+  genreDropdown.addEventListener('click', function(event) {
+    const genre = event.target.getAttribute('data-genre');
+    playAudio(genre);
   });
 });
